bdf8cfa440a54ffc345a1c0b8879141034d8dcf1
Mattie43 <ericksenm93@ymail.com>
2020-09-07 18:10:57 -0400

    images added

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -2,5 +2,6 @@ class RestaurantController < ApplicationController
   def show
     # byebug
     @restaurant = YelpModel.specific_search(params[:id])
+    @img_rating = Restaurant.img_rating(@restaurant.business.rating)
   end
 end
