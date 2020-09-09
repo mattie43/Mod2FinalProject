e9f23e20a0729a6ba46d1ed28ee5de544574d205
github username <github email address>
2020-09-09 18:28:22 -0400

    Home page/user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ApplicationRecord
     
     has_secure_password
 
+    def average_rating
+        if self.meals.count > 0
+            total_rating = self.meals.sum {|meal| meal.rating}
+            number_of_ratings = self.meals.count
+            total_rating / number_of_ratings
+        end
+    end
+
+    def highest_rating
+        highest = self.meals.max_by {|meal| meal.rating}
+        self.meals.find_by(rating: highest)
+    end
+
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environemnt.plugins.append('Provide',
+    new webpack.ProviderPlugin({
+        s: 'jquery',
+        jQuery: 'jquery'
+        Popper: ['popper.js', 'default']
+    })
+    )
+
 module.exports = environment
