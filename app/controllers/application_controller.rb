6b52e429045fca08aa0bb0fe97286abddc55323a
github username <github email address>
2020-09-10 09:55:37 -0400

    Home page and user page styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
 
     def logged_in_user
         @current_user = User.find_by(id: session[:user_id])
-      end 
+    end 
+
+    
     
     
     # def authorized
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def home
+    @meal = Meal.all.sample
   end
 end
